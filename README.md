a244a87c552b5c5b862717941dd3ca682afbd776
VasoBolkvadze <vaso.bolkvadze@gmail.com>
2014-03-11 14:31:41 +0400

    new release version 0.2

--- README.md
+++ README.md
@@ -2,14 +2,11 @@
 ###a simplified ravendb library for nodejs.
 ##usage
 ###var raven = require('nodeRaven');
-###var store = raven('http://ipaddress:port','dbname');
+###var store = raven('http://<server address>:<port>');
 ##api
-* store.<b>indexQuery</b>(indexName,luceneQuery,start,limit,sortBys,cb)
-  * indexName : string (name of the index)
-  * luceneQuery : string (where clause, lucene syntax)
-  * start : int (start index of records)
-  * limit : int (count of records to be retrieved)
-  * sortBys : array\<string\> (orderby column names)
-  * cb : function(err,result) - result : object {docs,stats}
-* store.<b>suggest</b>(indexName,term,fieldName,cb)
-* store.<b>save</b>(id,document,metadata,cb);
+* store.<b>indexQuery</b>(db, index, whereClause, start, limit, sortBys, fetchings, cb)
+* store.<b>suggest</b>(db, index, term, field, cb)
+* store.<b>load</b>(db, id, cb)
+* store.<b>update</b>(db, id, doc, meta, cb)
+* store.<b>patch</b>(db, id, operations, cb)
+* store.<b>store</b>(db, entityName, doc, cb)
--- index.js
+++ index.js
@@ -1,36 +1,47 @@
 var request = require('request');
 var _ = require('underscore');
-function bulkPost(host, db, operations, cb) {
-	request.post({
-		url: host + 'databases/' + db + '/bulk_docs',
-		headers:{
-			'Content-Type': 'application/json; charset=utf-8'
-		},
-		body: JSON.stringify(operations)
-	}, function (error, response, resBody) {
-		if (!error && response.statusCode === 200) {
-			var result = JSON.parse(resBody);
-			cb(null, result);
-		} else {
-			cb(error || response.statusCode, null);
-		}
-	});
-}
-module.exports = function(host,db){
+
+module.exports = function (host) {
 	return {
-		indexQuery:function(index,whereClause,start,limit,sortBys,cb){
+		indexQuery: function (db, index, whereClause, start, limit, sortBys, fetchings, cb) {
+			if (typeof whereClause === 'function') {
+				cb = whereClause;
+				whereClause = '';
+				start = 0;
+				limit = 12;
+				sortBys = [];
+				fetchings = [];
+			}
+			if (typeof start === 'function') {
+				cb = start;
+				start = 0;
+				limit = 12;
+				sortBys = [];
+				fetchings = [];
+			}
+			if (typeof sortBys === 'function') {
+				cb = sortBys;
+				sortBys = [];
+				fetchings = [];
+			}
+			if (typeof fetchings === 'function') {
+				cb = fetchings;
+				fetchings = [];
+			}
 			var url = host +
 				'databases/' + db +
-				'/indexes/'+ index +
-				'?&query='+ encodeURIComponent(whereClause) +
-				'&operator=AND'+
-				'&pageSize='+limit+
-				'&start='+start;
-			var sorts = sortBys.map(function(prop){
-				return '&sort='+prop;
+				'/indexes/' + index +
+				'?query=' + encodeURIComponent(whereClause) +
+				'&operator=AND' +
+				'&pageSize=' + limit +
+				'&start=' + start;
+			var sorts = sortBys.map(function (prop) {
+				return '&sort=' + prop;
 			}).join('');
-			url = url + sorts;
-			console.log(url);
+			var fetchs = fetchings.map(function (prop) {
+				return '&fetch=' + prop;
+			}).join('');
+			url = url + sorts + fetchs;
 			request(url, function (error, response, body) {
 				if (!error && response.statusCode === 200) {
 					var result = JSON.parse(body);
@@ -40,41 +51,112 @@ module.exports = function(host,db){
 						}
 						return memo;
 					}, {});
-					cb(null, {docs:result.Results,stats:stats});
+					cb(null, {docs: result.Results, stats: stats});
 				}
 				else {
 					cb(error || response.statusCode, null);
 				}
 			});
 		},
-		suggest:function(index,term,field,cb){
+		suggest: function (db, index, term, field, cb) {
 			var url = host +
 				'databases/' + db +
-				'/suggest/'+ index +
-				'?term='+ encodeURIComponent(term) +
-				'&field='+ field +
+				'/suggest/' + index +
+				'?term=' + encodeURIComponent(term) +
+				'&field=' + field +
 				'&max=10&distance=Default&accuracy=0.5';
 			request(url, function (error, response, body) {
 				if (!error && response.statusCode === 200) {
 					var result = JSON.parse(body);
-					console.log(result);
-					cb(result.Suggestions);
+					cb(null, result);
 				}
 				else {
 					cb(error || response.statusCode, null);
 				}
 			});
 		},
-		save:function(id,doc,meta,cb){
+		load: function (db, id, cb) {
+			var url = host + 'databases/' + db + '/docs/' + id;
+			request(url, function (error, response, body) {
+				if (!error && response.statusCode === 200) {
+					var doc = JSON.parse(body);
+					var meta = _.reduce(response.headers
+						, function (memo, val, key) {
+							if (key.startsWith('raven'))
+								memo[key] = val;
+							return memo;
+						}, {});
+					meta['@id'] = response.headers['__document_id'];
+					meta.etag = response.headers['etag'];
+					doc['@metadata'] = meta;
+					cb(null, doc);
+				} else {
+					cb(error || response.statusCode, null);
+				}
+			});
+		},
+		update: function (db, id, doc, meta, cb) {
 			var operations = [
 				{
 					Method: "PUT",
-					Document:doc,
-					Metadata:meta,
+					Document: doc,
+					Metadata: meta,
 					Key: id
 				}
 			];
-			bulkPost(host,db,operations,cb);
+			request.post({
+				url: host + 'databases/' + db + '/bulk_docs',
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8'
+				},
+				body: JSON.stringify(operations)
+			}, function (error, response, resBody) {
+				if (!error && response.statusCode === 200) {
+					var result = JSON.parse(resBody);
+					cb(null, result);
+				} else {
+					cb(error || response.statusCode, null);
+				}
+			});
+		},
+		/** Applies patch operations to given document.
+		 * @param {string} db
+		 * @param {string} id
+		 * @param {Array<Object>} operations
+		 * @param {Function} cb
+		 */
+		patch: function (db, id, operations, cb) {
+			request.patch({
+				url: host + 'databases/' + db + '/docs/' + id,
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8'
+				},
+				body: JSON.stringify(operations)
+			}, function (error, response, resBody) {
+				if (!error && response.statusCode === 200) {
+					var result = JSON.parse(resBody);
+					cb(null, result);
+				} else {
+					cb(error || response.statusCode, null);
+				}
+			});
+		},
+		store: function (db, entityName, doc, cb) {
+			request.post({
+				url: host + 'databases/' + db + '/docs',
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8',
+					'Raven-Entity-Name': entityName
+				},
+				body: JSON.stringify(doc)
+			}, function (error, response, resBody) {
+				if (!error && response.statusCode === 201) {
+					var result = JSON.parse(resBody);
+					cb(null, result);
+				} else {
+					cb(error || new Error(response.statusCode), null);
+				}
+			});
 		}
 	}
 };
